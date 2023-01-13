1a7a4d379431be811fab7116efa286e2fd952793
Niels bakmann <niebak@hotmail.no>
2023-01-13 14:37:52 +0100

    tested DTW but wasnt as usefull as id hoped

--- DTW_testing.py
+++ DTW_testing.py
@@ -1,11 +1,35 @@
+
 import pandas as pd
-# from dtw import dtw
+
+from dtw import dtw
 import pyarrow as pa
 import pyarrow.parquet as pq
-
+import numpy as np
+import matplotlib.pyplot as plt
+print('testtest')
 coursename = "data/221005_eksempelsegment001.xlsx"
 parquetfile='data/2022-06-05-12-12-09 (1).parquet'
 
-trail1=pd.read_csv(coursename)
-trail2=pd.read_parquet(parquetfile)
-print(trail1,trail2)
+Columns=['position_lat','position_long','altitude']
+
+# Read data
+TDF0=pd.read_excel(coursename)[Columns]
+print(TDF0.columns)
+TDF1=pd.read_parquet(parquetfile)[Columns]
+print(TDF1.columns)
+distance = dtw(TDF0.values, TDF1.values)
+
+distance.plot()
+
+fig=plt.figure()
+ax=fig.add_subplot(1,1,1)
+# ax.plot(distance.index1,distance.index2)
+# plt.show()
+
+ldist = np.ones((6,6))                    # Matrix of ones
+ldist[1,:] = 0; ldist[:,4] = 0           # Mark a clear path of zeroes
+ldist[1,4] = .01
+
+ds = dtw(ldist)
+ax.plot(ds.index1,ds.index2)
+plt.show()
