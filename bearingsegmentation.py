093a99021f3363250b80c6b614c05f76f083a8a1
Niels bakmann <niebak@hotmail.no>
2023-01-18 20:46:46 +0100

    Looking at segmentation using bearing

--- bearingsegmentation.py
+++ bearingsegmentation.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from tabulate import tabulate
 import sys
 sys.path.append('../DIVCODE')
 from code_functions import *
@@ -7,15 +8,20 @@ from code_functions import *
 coursename = "data/221005_eksempelsegment001.xlsx" # Short, but with a lot of data
 
 # Load data into a dataframe
-df=pd.read_excel(coursename)
+# df=pd.read_excel(coursename)
+ownfile='data/Evening_Run.fit'
+variables = ['position_lat', 'position_long', 'altitude','distance']
+
+tdf=fit_records_to_frame(ownfile,variables)
+df=tdf[variables].dropna()
 # Add a new column for bearing
 df['position_lat'] = df['position_lat']/(2**32/360)
 df['position_long'] = df['position_long']/(2**32/360)
 df['bearing'] = np.nan
 
 #set sensitivity
-sensitivity = 1 # change this value to adjust sensitivity
-alt_sensitivity = 5 # change this value to adjust sensitivity
+sensitivity = 10 # change this value to adjust sensitivity
+alt_sensitivity = 0 # change this value to adjust sensitivity
 
 # Loop through the dataframe to calculate the bearing at each point
 for i in range(1, len(df)):
@@ -35,17 +41,21 @@ for i in range(1, len(df)):
 # Add new column for segments
 df['segments'] = 0
 
+markers=detect_and_mark_change_in_direction(df['bearing'].tolist(),df['incline'].tolist(),df['segments'])
+segments=marker_to_segment(markers)
+df['segments'] = segments
+
 # loop through the dataframe to segment the data
-segment_count = 0
-for i in range(1, len(df)):
-    if abs(df.loc[i, 'bearing'] - df.loc[i-1, 'bearing']) > sensitivity:
-        segment_count += 1
-        df.loc[i, 'segments'] = segment_count
-    else:
-        if abs(df.loc[i, 'incline'] - df.loc[i-1, 'incline']) > sensitivity:
-            segment_count += 1
-            df.loc[i, 'segments'] = segment_count
-        else:
-            df.loc[i, 'segments'] = segment_count
-print(df)
+# segment_count = 0
+# for i in range(1, len(df)):
+#     if abs(df.loc[i, 'bearing'] - df.loc[i-1, 'bearing']) > sensitivity:
+#         segment_count += 1
+#         df.loc[i, 'segments'] = segment_count
+#     else:
+#         if abs(df.loc[i, 'incline'] - df.loc[i-1, 'incline']) > sensitivity:
+#             segment_count += 1
+#             df.loc[i, 'segments'] = segment_count
+#         else:
+#             df.loc[i, 'segments'] = segment_count
+print(tabulate(df[['segments','bearing','incline']],headers='keys',tablefmt='github'))
 a,b=plot_segments_and_trail(df,Show_plot=True)
