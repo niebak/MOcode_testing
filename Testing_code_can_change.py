7883bfaa4d78e65c83e02aabcf17d04e7d83ab36
Niels bakmann <niebak@hotmail.no>
2023-01-18 08:10:45 +0100

    This script is for testing code

--- Testing_code_can_change.py
+++ Testing_code_can_change.py
@@ -17,13 +17,10 @@ parquetfile='data/2022-06-05-12-12-09 (1).parquet' # Longer, from strava
 ownfile='data/Evening_Run.fit'
 
 TDF=fit_records_to_frame(ownfile,variables)
-# TDF.drop(TDF.index[:200], inplace=True)
-TDF2=TDF[['position_lat','position_long','altitude']].dropna()
-
-# TDF2=TDF2
-# Read data
 TDF0=pd.read_excel(coursename)
-#TDF1=pd.read_parquet(parquetfile)
+TDF1=pd.read_parquet(parquetfile)
+TDF2=TDF[['position_lat','position_long','altitude']].dropna()
+#  Read data
 Weird_format=True # If the data is from the xlsx file, it is in a weird format so we need to change this
 if Weird_format:
     # The data is in a weird format so we need to change it:
@@ -34,14 +31,17 @@ if Weird_format:
 
 # Create the vector-representation coordinates
 TDF0_vector = coordinate_to_vector_dataframe(TDF0)
+TDF1_vector = coordinate_to_vector_dataframe(TDF1)
 TDF2_vector = coordinate_to_vector_dataframe(TDF2)
-print(len(TDF2_vector))
-#TDF1_vector = coordinate_to_vector_dataframe(TDF1)
+
 TDF0_seg_marker=detect_and_mark_change_in_direction(
     TDF0_vector['Vposition_lat'].tolist(),
     TDF0_vector['Vposition_long'].tolist(),
     TDF0_vector['Valtitude'].tolist())
-
+TDF1_seg_marker=detect_and_mark_change_in_direction(
+    TDF1_vector['Vposition_lat'].tolist(),
+    TDF1_vector['Vposition_long'].tolist(),
+    TDF1_vector['Valtitude'].tolist())
 TDF2_seg_marker=detect_and_mark_change_in_direction(
     TDF2_vector['Vposition_lat'].tolist(),
     TDF2_vector['Vposition_long'].tolist(),
@@ -51,13 +51,13 @@ TDF0_seg =  marker_to_segment(TDF0_seg_marker)
 TDF0=pd.concat([TDF0,TDF0_vector],axis=1)
 TDF0['segments']=TDF0_seg
 
-TDF2_seg =  marker_to_segment(TDF2_seg_marker)
-print(len(TDF2_vector))
-TDF2=pd.concat([TDF2,TDF2_vector],axis=1)
-# print(len(TDF2))
 
+TDF1_seg =  marker_to_segment(TDF1_seg_marker)
+TDF1=pd.concat([TDF1,TDF1_vector],axis=1)
+TDF1['segments']=TDF1_seg
+
+TDF2_seg =  marker_to_segment(TDF2_seg_marker)
+TDF2=pd.concat([TDF2,TDF2_vector],axis=1)
 TDF2['segments']=TDF2_seg
 
-for segment in np.unique(TDF0['segments'].tolist()):
-    wdf=TDF2[['Vposition_lat','Vposition_long','Valtitude']].loc[TDF2['segments']==segment]
-    print(f'\nSegment {segment} '+str(wdf.describe().loc[['mean','std','count']]))
+print(TDF0_seg[-1],TDF1_seg[-1],TDF2_seg[-1])
