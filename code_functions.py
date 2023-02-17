Correct rotation and moving the points to a signal

code_functions.py
@@ -573,13 +573,12 @@ def segments_to_feature_df_with_rev(TDF0):
     featdict={'segments':np.unique(segments),'curvature':curvature,'revcurvature':[i * -1 for i in curvature],'climb':climb,'revclimb':[i * -1 for i in climb],'seg_distance':seg_dist}
     featureDF=pd.DataFrame(featdict)
     return featureDF
-def print_df(df,headers='keys',tablefmt='github'):
+def print_df(df,headers='keys',tablefmt='github',length=10):
     if 'tabulate' not in globals():
         from tabulate import tabulate
-    if df.shape[0]<11:
+    if df.shape[0]<length:
         length = df.shape[0]
-    else:
-        length=10
+    
     print('\n')
     print(tabulate(df.iloc[0:length], headers=headers, tablefmt=tablefmt))
     print('\n')

rotation.py
@@ -1,28 +1,87 @@
 import pandas as pd
-from code_functions import DF_to_segmented_DF,print_df
+from code_functions import DF_to_segmented_DF,print_df,fit_records_to_frame,plot_segments_and_trail
 import numpy as np
 import matplotlib.pyplot as plt
 
+def angle_with_x_axis(start, end):
+    """
+    Computes the angle (in radians) between the vector going from start to end and the x-axis.
+    """
+    if 'math' not in globals():
+        import math
+    # Compute the vector between the start and end points
+    x = end[0] - start[0]
+    y = end[1] - start[1]
+    
+    # Compute the angle between the vector and the x-axis
+    angle = math.atan2(y, x)
+    
+    return angle
+def dist(x, y, latline, lonline):
+    if 'math' not in globals():
+        import math
+    dists = []
+    for i in range(len(x)):
+        closest_dist = float('inf')
+        closest_lat, closest_lon = None, None
+        for j in range(len(latline)-1):
+            x1, y1 = latline[j], lonline[j]
+            x2, y2 = latline[j+1], lonline[j+1]
+            xp = x1 + (x2 - x1) * ((x[i] - x1) * (x2 - x1) + (y[i] - y1) * (y2 - y1)) / ((x2 - x1)**2 + (y2 - y1)**2)
+            yp = y1 + (y2 - y1) * ((x[i] - x1) * (x2 - x1) + (y[i] - y1) * (y2 - y1)) / ((x2 - x1)**2 + (y2 - y1)**2)
+            dist = math.sqrt((xp - x[i])**2 + (yp - y[i])**2)
+            vec1 = [xp - x[i], yp - y[i], 0]
+            vec2 = [x2 - x1, y2 - y1, 0]
+            cross = np.cross(vec1, vec2)
+            if cross[2] > 0:
+                dist *= -1  # point is above line
+            closest_dist = dist if dist < closest_dist else closest_dist
+            closest_lat = xp if dist < closest_dist else closest_lat
+            closest_lon = yp if dist < closest_dist else closest_lon
+        dists.append(closest_dist)
+    return dists
+
+
 TDF0 = DF_to_segmented_DF(pd.read_excel('data/221005_eksempelsegment001.xlsx'))
+# TDF0 = DF_to_segmented_DF(fit_records_to_frame('data/Evening_Run.fit'))
+# plot_segments_and_trail(TDF0,x_axis='Vposition_long',y_axis='Vposition_lat',Show_Plot=True)
 
-# Define some sample data
-x = np.linspace(0, 2*np.pi, 100)
-y = np.sin(x)
 
-# Plot the original graph
-plt.plot(x, y)
-plt.title('Original Graph')
 
-# Rotate the graph
-angle = np.pi/2  # 45 degrees
-rotation_matrix = np.array([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]])
-xy = np.column_stack((x, y))
-rotated_xy = np.dot(xy, rotation_matrix)
-rotated_x, rotated_y = rotated_xy[:,0], rotated_xy[:,1]
+for i in np.unique(TDF0['segments'].to_numpy()):
+    wdf = TDF0.loc[TDF0['segments']==i].copy(deep=True)
+    # wdf['position_lat']=wdf['Vposition_lat'].cumsum()
+    # wdf['position_long']=wdf['Vposition_long'].cumsum()
+    
+    # plot_segments_and_trail(wdf,Show_Plot=True)
+    
+    x = wdf['position_lat'].to_numpy()
+    y = wdf['position_long'].to_numpy()
+    latline = np.linspace(x[0],x[-1],len(x))
+    lonline = np.linspace(y[0],y[-1],len(y))
+    
+    ax0,ax1 = plot_segments_and_trail(wdf)
+    ax1.plot(lonline,latline)
+    distances = dist(x,y,latline,lonline)
+    print(distances)
+    plt.show()
+    # Rotate the graph
+    angle = angle_with_x_axis((latline[0],lonline[0]),(latline[-1],lonline[-1]))  # 45 degrees
+    print(angle)
+    rotation_matrix = np.array([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]])
+    xy = np.column_stack((x, y))
+    rotated_xy = np.dot(xy, rotation_matrix)
+    rotated_x, rotated_y = rotated_xy[:,0], rotated_xy[:,1]
 
-# Plot the rotated graph
-plt.figure()
-plt.plot(rotated_x, rotated_y)
-plt.title('Rotated Graph')
-
-plt.show()
+    #Plot the original graph
+    fig=plt.figure()
+    ax2=fig.add_subplot(2,1,1)
+    ax3=fig.add_subplot(2,1,2)
+    ax2.plot(x, y)
+    # ax2.plot(lonline,latline)
+    ax2.set_title('Original Graph')
+    #Plot the rotated graph
+    # ax3.plot(rotated_x, rotated_y)
+    ax3.stem(distances)
+    ax3.set_title('stemmed Graph')
+    plt.show()

spectrogram_creation.py
@@ -44,7 +44,6 @@ def rotate_and_curve_amplitude(df):
         theta = np.arctan((y2 - y1) / (x2 - x1))
     else:
         theta = 0
-    print(theta)
     # Apply rotation matrix to each point in the trail
     R = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
     coords = df[['position_long', 'position_lat']].to_numpy()
@@ -93,42 +92,99 @@ def freq_analysis(df):
     signal2_db = 20 * np.log10(signal2_magnitude)
     df['climb_freq'] = climb_freq
     df['climb_fft_db']=signal2_db
+def spectrogram_correlation(mag1,mag2):
+    if 'scipy' not in globals():
+        import scipy as sc
+    if mag1.shape==mag2.shape:
+        correlation = sc.signal.correlate(mag1,mag2)
+        return correlation
+    else:
+        window_size = mag1.shape[1]
+        shift_size = mag2.shape[1] - window_size
+        print('shift size',shift_size)
+        for i in range(0,shift_size):
+            mag2_window=mag2[:,i:i+window_size]
+            print((i,i+window_size))
+            print(mag2_window,'\n')
+def split_freq_bands(spectrogram, freq_list, bottom_freq, top_freq, num_bins):
+    """
+    Split the frequency bands of a spectrogram into evenly spaced bins.
 
+    Args:
+        spectrogram (np.ndarray): Magnitude spectrogram matrix (size: len(freq_list) x len(time)).
+        freq_list (np.ndarray): List of frequencies corresponding to rows of the spectrogram.
+        bottom_freq (float): Bottom frequency value for the lowest bin.
+        top_freq (float): Top frequency value for the highest bin.
+        num_bins (int): Number of bins to split the frequency range into.
 
+    Returns:
+        freq_bins (np.ndarray): List of frequencies for each bin (size: num_bins).
+        energy (np.ndarray): Energy in each frequency bin for each time step in the spectrogram 
+            (size: num_bins x len(time)).
+    """
 
-# TDF0 = DF_to_segmented_DF(pd.read_excel('data/221005_eksempelsegment001.xlsx'))
+    # Calculate the frequency range of each bin
+    freq_range = np.linspace(bottom_freq, top_freq, num_bins+1)
+    freq_bins = (freq_range[1:] + freq_range[:-1])/2
+
+    # Find the indices of the frequency range of each bin in the spectrogram
+    bin_indices = np.searchsorted(freq_list, freq_range)
+
+    # Split the spectrogram into frequency bins
+    energy = np.zeros((num_bins, spectrogram.shape[1]))
+    for i in range(num_bins):
+        bin_energy = np.sum(spectrogram[bin_indices[i]:bin_indices[i+1], :], axis=0)
+        energy[i, :] = bin_energy
+
+    return freq_bins, energy
+
+TDF0 = DF_to_segmented_DF(pd.read_excel('data/221005_eksempelsegment001.xlsx'))
 file = 'data/Evening_Run.fit'
-TDF0 = DF_to_segmented_DF(fit_records_to_frame(file)).iloc[200:400]
+# TDF0 = DF_to_segmented_DF(fit_records_to_frame(file)).iloc[200:400]
+TDF1 = DF_to_segmented_DF(fit_records_to_frame(file)).iloc[200:600]
 # plot_segments_and_trail(TDF0,Show_Plot=True)
-# print_df(TDF0)
+
 avrg_samplefreq = 1/(np.mean(TDF0['seconds']))
 sample_rate = avrg_samplefreq
-# print(sample_rate)
+
 amps,coords_rot = rotate_and_curve_amplitude(TDF0)
 signal = np.array(amps)
 window_size = 60 #sum(TDF0['seconds'])/(len(np.unique(TDF0['segments'])))  # seconds
 window_samples = int(window_size * sample_rate)
 overlap = window_samples // 2
-# print(window_size,window_samples,overlap,type(signal),sample_rate)
 f, t, Sxx = spectrogram(signal, fs=sample_rate, window='hann', nperseg=window_samples, noverlap=overlap, scaling='spectrum')
-
-# print(sum(TDF0['seconds']))
-
 fig3, ax3 = plt.subplots(figsize=(6, 4))
 ax3.pcolormesh(t, f, np.log10(Sxx), cmap='inferno')
-ax3.set_ylim([0, 0.5])
+ax3.set_ylim([0, 0.15])
 ax3.set_xlabel('Time (s)')
 ax3.set_ylabel('Frequency (Hz)')
-ax3.set_title('Spectrogram')
-# ax3.grid()
+ax3.set_title('Spectrogram for first section')
 
-plt.show()
+avrg_samplefreq1 = 1/np.mean(TDF1['seconds'])
+sample_rate1 = avrg_samplefreq1
+window_samples1 = int(window_size * sample_rate1)
+overlap1 = window_samples1 // 2
+amps1,coords_rot1 = rotate_and_curve_amplitude(TDF1)
+signal1 = np.array(amps1)
+f1, t1, Sxx1 = spectrogram(signal1, fs=sample_rate1, window='hann', nperseg=window_samples1, noverlap=overlap1, scaling='spectrum')
+fig1, ax1 = plt.subplots(figsize=(6, 4))
 
-# freq_analysis(TDF0)
-# freqfig=plt.figure()
-# frexax1=freqfig.add_subplot(2,1,1)
-# frexax2=freqfig.add_subplot(2,1,2)
-# frexax1.plot(TDF0['position_freq'],TDF0['lat_fft_db'])
-# frexax1.plot(TDF0['position_freq'],TDF0['long_fft_db'])
-# frexax2.plot(TDF0['climb_freq'],TDF0['climb_fft_db'])
+ax1.pcolormesh(t1, f1, np.log10(Sxx1), cmap='inferno')
+ax1.set_ylim([0, 0.15])
+ax1.set_xlabel('Time (s)')
+ax1.set_ylabel('Frequency (Hz)')
+ax1.set_title('Spectrogram for second section')
 # plt.show()
+
+# print('Window samples 1',window_samples1)
+# print('Window samples',window_samples)
+
+start_freq=0
+end_freq=0.135
+num_bins=15
+
+bin_edge0,bin_energy0 = split_freq_bands(Sxx,f,start_freq,end_freq,num_bins)
+bin_edge1,bin_energy1 = split_freq_bands(Sxx1,f1,start_freq,end_freq,num_bins)
+temp = spectrogram_correlation(bin_energy0,bin_energy1)
+print(bin_energy0.shape,bin_energy1.shape)
+print(temp)